b9f4b8c71af3bb45223462f43d82ccf463c76c5d
Celofyz <thecelofyz@gmail.com>
2018-12-09 13:35:05 +0100

    auto running in browser

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var http = require('http');
 var dt = require('./testModule');
+var opn = require('opn');
 
-http.createServer(function (req, res){
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+function onRequest (req, res){
+  res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("Current Time and Date: " + dt.myDateTime())
   res.end();
-}).listen(8080);
+}
+
+http.createServer(onRequest).listen(8080);
+opn('http://localhost:8080', {app: 'firefox'})
