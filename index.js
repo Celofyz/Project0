0c7ce86e0b0b332cb636e2b3c3f6cc3ee00413ab
Celofyz <thecelofyz@gmail.com>
2018-12-09 15:53:20 +0100

    removed counting

--- index.js
+++ index.js
@@ -3,27 +3,13 @@ var dt = require('./testModule');
 var opn = require('opn');
 var fs = require('fs');
 
-var i = 0;
-
-
-// function onRequest (req, res){
-//   fs.readFile('body.html', function(err, data){
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-//     res.write(data);
-//     res.end();
-//   });
-// }
-
-function iterRequest (req, res){
-  if(req.url != '/favicon.ico'){
-    i++;
-    console.log('request');
-  }
-
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write(i.toString());
-  res.end();
+function onRequest (req, res){
+  fs.readFile('body.html', function(err, data){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
 }
 
-http.createServer(iterRequest).listen(8080);
-opn('http://localhost:8080', {app: 'firefox'})
+http.createServer(onRequest).listen(8080);
+opn('http://localhost:8080', {app: 'firefox'});
