ea4ea819d296793d8123928793f3a00a370788fc
Celofyz <thecelofyz@gmail.com>
2018-12-10 19:47:05 +0100

    update

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ var http = require('http');
 var dt = require('./testModule');
 var opn = require('opn');
 var fs = require('fs');
+var express = require('express');
+var app = express();
+
+app.post('/sendamino', function(req, res){
+  console.log('post');
+  console.log(req);
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(data);
+  res.end();
+});
 
 function onRequest (req, res){
   fs.readFile('body.html', function(err, data){
@@ -9,6 +19,7 @@ function onRequest (req, res){
     res.write(data);
     res.end();
   });
+  if(req.title !== null) console.log(req);
 }
 
 http.createServer(onRequest).listen(8080);
