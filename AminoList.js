98d3797b4ad3834ff49ec526fc769a0c962b9e0c
Celofyz <thecelofyz@gmail.com>
2018-12-11 19:24:08 +0100

    hotfix

--- AminoList.js
+++ AminoList.js
@@ -9,7 +9,7 @@ module.exports = {
     var list = '';
 
     for(var i = 0; i < amiList.length; i++){
-      if(i != amiList.length - 1{
+      if(i != amiList.length - 1){
         list = list + "<a>" + amiList[i] + "</a>" + "<br>";
       }else{
         list = list + "<a>" + amiList[i] + "</a>" + "<br>"; //ostatni element
