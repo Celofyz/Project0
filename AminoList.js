17704bd585aaa6bf12b4c99d69973b43f217391c
Pecetson <ryuusakimahirojz@gmail.com>
2018-12-11 20:20:06 +0100

    Animacja pojawiania się plików na liście.

--- AminoList.js
+++ AminoList.js
@@ -10,7 +10,7 @@ module.exports = {
 
     for(var i = 0; i < amiList.length; i++){
       if(i != amiList.length - 1){
-        list = list + "<a>" + amiList[i] + "</a>" + "<br>";
+        list = list + "<a id='notlast'>" + amiList[i] + "</a>" + "<br>";
       }else{
         list = list + "<a>" + amiList[i] + "</a>" + "<br>"; //ostatni element
       }
