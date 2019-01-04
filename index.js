bd9cc1d86831f6adfb72d13fbf075ecdc3df684b
Pecetson <ryuusakimahirojz@gmail.com>
2019-01-04 20:42:31 +0100

    Dosłownie minimalna zmiana w kodzie

--- index.js
+++ index.js
@@ -104,12 +104,6 @@ function onRequest (req, res){
         fs.readFile(htmlPath, function(err, data){
           res.writeHead(200, {'Content-Type': 'text/html'});
           res.write(data);
-          //Test kod - trzeba machnąć module prompt, bo takiego nie mam. Ale ja nie umiem tu w pliczki to zostawiam Tobie.
-          /*prompt.start();
-          prompt.get(['username'], function(err, result){
-            console.log(result.username);
-          });*/
-          //Test kod end
           res.write(aminoList.get());
           res.end();
         });
